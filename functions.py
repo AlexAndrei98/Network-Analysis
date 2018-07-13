7182db0cc4a751bae1c621f993209d7ab163923f
Alexandru Andrei <alexandru.andrei@rgare.com>
2018-07-13 16:13:10 -0500

    added files

---
+++ functions.py
@@ -0,0 +1,11 @@
+# Define get_nodes_from_partition()
+def get_nodes_from_partition(G, partition):
+    # Initialize an empty list for nodes to be returned
+    nodes = []
+    # Iterate over each node in the graph G
+    for n in G.nodes():
+        # Check that the node belongs to the particular partition
+        if G.node[n]['bipartite'] == partition:
+            # If so, append it to the list of nodes
+            nodes.append(n)
+    return nodes
